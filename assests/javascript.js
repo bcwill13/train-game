bad14d376a9b01adc5825f18dc849adfcd0f156d
bcwill <Brigit@brigits-mbp.home>
2019-10-14 19:28:39 -0400

    train-game

--- assests/javascript.js
+++ assests/javascript.js
@@ -12,26 +12,77 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-var trainName = "";
-var destination = "";
-var firstTrainTime = "";
+var name;
+var destination;
+var firstTrain;
 var frequency = 0;
-var currentTime = moment();
-var index = 0;
-var trainIDs = [];
 
-var datetime = null,
-  date = null;
+$("#add-train").on("click", function() {
+  event.preventDefault();
+  name = $("#train-name")
+    .val()
+    .trim();
+  destination = $("#destination")
+    .val()
+    .trim();
+  firstTrain = $("#first-train")
+    .val()
+    .trim();
+  frequency = $("#frequency")
+    .val()
+    .trim();
 
-var update = function() {
-  date = moment(new Date());
-  datetime.html(date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-};
-
-$(document).ready(function() {
-  datetime = $("#current-status");
-  update();
-  setInterval(update, 1000);
+  database.ref().push({
+    name: name,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+  $("form")[0].reset();
 });
 
+database.ref().on(
+  "child_added",
+  function(childSnapshot) {
+    var nextArr;
+    var minAway;
+    var firstTrainNew = moment(
+      childSnapshot.val().firstTrain,
+      "hh:mm"
+    ).subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+    var remainder = diffTime % childSnapshot.val().frequency;
+    var minAway = childSnapshot.val().frequency - remainder;
+    var nextTrain = moment().add(minAway, "minutes");
+    nextTrain = moment(nextTrain).format("hh:mm");
 
+    $("#add-row").append(
+      "<tr><td>" +
+        childSnapshot.val().name +
+        "</td><td>" +
+        childSnapshot.val().destination +
+        "</td><td>" +
+        childSnapshot.val().frequency +
+        "</td><td>" +
+        nextTrain +
+        "</td><td>" +
+        minAway +
+        "</td></tr>"
+    );
+  },
+  function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  }
+);
+
+database
+  .ref()
+  .orderByChild("dateAdded")
+  .limitToLast(1)
+  .on("child_added", function(snapshot) {
+    $("#name-display").html(snapshot.val().name);
+    $("#email-display").html(snapshot.val().email);
+    $("#age-display").html(snapshot.val().age);
+    $("#comment-display").html(snapshot.val().comment);
+  });
